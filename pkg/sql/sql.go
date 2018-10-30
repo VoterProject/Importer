0c82a30340c18d5cf8f65e11295a9e71d9349faa
Amir Omidi <amir@aaomidi.com>
2018-10-29 22:50:17 -0400

    SQL stuff

--- pkg/sql/sql.go
+++ pkg/sql/sql.go
@@ -1,1 +1,20 @@
 package sql
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+type voterdb struct {
+	DB *sql.DB
+}
+
+func NewSQL(connectionString string) *voterdb {
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &voterdb{DB: db}
+}
