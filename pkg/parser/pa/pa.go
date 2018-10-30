844d84ede9bc9c9dd6265ffe2299999a8be9a3c4
Amir Omidi <amir@aaomidi.com>
2018-10-29 22:38:02 -0400

    Types, etc.

--- pkg/parser/pa/pa.go
+++ pkg/parser/pa/pa.go
@@ -3,14 +3,23 @@ package pa_parser
 import (
 	"bufio"
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 )
 
 func ParseDirectory(path string) {
 	fmt.Println(path)
 
 	filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if !strings.Contains(path, "FVE") {
+			return nil
+		}
+		if !strings.Contains(path, "MONTGOMERY") {
+			return nil
+		}
 		parseFile(path)
 		return nil
 	})
@@ -25,10 +34,67 @@ func parseFile(path string) {
 
 	for scanner.Scan() {
 		parseLine(scanner.Text())
+		//return
 	}
 
 }
 
-func parseLine(line string) {
+func parseLine(line string) *Record {
+	fields := strings.Split(line, "\t")
 
+	record := Record{
+		parseString(fields[0]),
+		parseString(fields[1]),
+		parseString(fields[2]),
+		parseString(fields[3]),
+		parseString(fields[4]),
+		parseString(fields[5]),
+		parseString(fields[6]),
+		parseTime(fields[7]),
+		parseTime(fields[8]),
+		parseString(fields[9]),
+		parseTime(fields[10]),
+		parseString(fields[11]),
+		parseString(fields[12]),
+		parseString(fields[13]),
+		parseString(fields[14]),
+		parseString(fields[15]),
+		parseString(fields[16]),
+		parseString(fields[17]),
+		parseString(fields[18]),
+		parseString(fields[19]),
+		parseString(fields[20]),
+		parseString(fields[21]),
+		parseString(fields[22]),
+		parseString(fields[23]),
+		parseString(fields[24]),
+		parseTime(fields[25]),
+		parseString(fields[26]),
+		parseString(fields[27]),
+		parseTime(fields[28]),
+		parseString(fields[29]),
+		parseString(fields[150]),
+		parseString(fields[151]),
+		parseString(fields[152]),
+	}
+
+	if record.FirstName != nil && *(record.FirstName) == "AMIR" {
+		spew.Println(record)
+	}
+	return &record
+}
+
+func parseTime(field string) *time.Time {
+	t, err := time.Parse("01/02/2006", field)
+	if err != nil {
+		return nil
+	}
+	return &t
+}
+func parseString(field string) *string {
+	field = strings.Trim(field, "\"")
+	if field == "" {
+		return nil
+	}
+	return &field
 }
---
+++ pkg/parser/pa/types.go
@@ -0,0 +1,39 @@
+package pa_parser
+
+import "time"
+
+type Record struct {
+	ID                *string
+	Title             *string
+	LastName          *string
+	FirstName         *string
+	MiddleName        *string
+	Suffix            *string
+	Gender            *string
+	DOB               *time.Time
+	RegistrationDate  *time.Time
+	VoterStatus       *string
+	StatusChangeDate  *time.Time
+	PartyCode         *string
+	HouseNumber       *string
+	HouseNumberSuffix *string
+	StreetName        *string
+	ApartmentNumber   *string
+	AddressLine2      *string
+	City              *string
+	State             *string
+	Zip               *string
+	MailAddress1      *string
+	MailAddress2      *string
+	MailCity          *string
+	MailState         *string
+	MailZip           *string
+	LastVoteDate      *time.Time
+	PrecinctCode      *string
+	PrecinctSplitID   *string
+	DateLastChanged   *time.Time
+	CustomData1       *string
+	HomePhone         *string
+	County            *string
+	MailCountry       *string
+}
